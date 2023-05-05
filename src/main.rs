8951d6946b873b90f8b0b2001e204047f50b2ff7
Boyaala <amin.boyaala@dedalus.com>
2023-05-05 09:51:14 +0200

    collision more comples

--- src/main.rs
+++ src/main.rs
@@ -62,7 +62,7 @@ async fn main() {
 struct Ball {
     pos: Vec2,
     sye: Vec2,
-    vel: Vec2,
+    vel: Vec2, 
 }
 
 impl Ball {
@@ -91,13 +91,39 @@ impl Ball {
     }
 
     fn collision(&mut self, player: &Player) {
-        if self.pos.x < player.pos.x + player.sye.x &&
-            self.pos.x + self.sye.x > player.pos.x &&
-            self.pos.y < player.pos.y + player.sye.y &&
+
+        let x1 = Vec2::new(player.pos.x, player.pos.x + player.sye.x / 5.0);
+        let x2 = Vec2::new(player.pos.x + player.sye.x / 5.0, player.pos.x + (player.sye.x / 5.0) * 4.0);
+        let x3 = Vec2::new(player.pos.x + ((player.sye.x / 5.0) * 4.0), player.pos.x + player.sye.x);
+
+        // is the ball on the right height
+        if self.pos.y < player.pos.y + player.sye.y &&
             self.pos.y + self.sye.y > player.pos.y
         {
-            self.vel.y *= -1.0;
+            // is the ball in the second quarter of the paddle
+            if self.pos.x < x1.y && self.pos.x + self.sye.x > x1.x {
+                self.vel.x -= 0.5;
+                self.vel.y = -1.0;
+            }
+            // is the ball in the third quarter of the paddle
+            if self.pos.x < x2.y && self.pos.x + self.sye.x > x2.x {
+                self.vel.y = -1.0;
+            }
+            // is the ball in the fourth quarter of the paddle
+            if self.pos.x < x3.y && self.pos.x + self.sye.x > x3.x {
+                self.vel.x += 0.5;
+                self.vel.y = -1.0;
+            }
+            self.vel.x = clamp(self.vel.x, -1.0, 1.0);
         }
+
+        // if self.pos.x < player.pos.x + player.sye.x &&
+        //     self.pos.x + self.sye.x > player.pos.x &&
+        //     self.pos.y < player.pos.y + player.sye.y &&
+        //     self.pos.y + self.sye.y > player.pos.y
+        // {
+        //     self.vel.y *= -1.0;
+        // }
     }
     
     fn draw(&self) {
